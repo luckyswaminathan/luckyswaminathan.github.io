70e58e05865ee646d23c64a5e54c3a2ff1336a6e
luckyswaminathan <lakshman.swaminathan1@gmail.com>
2024-03-10 17:54:24 -0400

    changes

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -5,12 +5,12 @@ const Sidebar = () => (
   <aside className="sidebar">
     <nav className="nav">
       <a className='fnav' href="/">About</a>
-      <a className='nav-link' href="/experience">Experience</a>
-      <a className='nav-link' href="/projects">Projects</a>
-      <a className='nav-link' href="/web3">Web3</a>
+      <a className='nav-link' href="https://luckyswaminathan.github.io/experience">Experience</a>
+      <a className='nav-link' href="https://luckyswaminathan.github.io/projects">Projects</a>
+      <a className='nav-link' href="https://luckyswaminathan.github.io/web3">Web3</a>
       <a className='nav-link' href="https://github.com/luckyswaminathan">Github</a>
       <a className='nav-link' href="https://drive.google.com/file/d/1p_cpahKoh0hIcLfgK0jwcIqcSsXa4J9A/view?usp=sharing">Resume</a>
-      <a className='nav-link' href="/contact">Contact</a>
+      <a className='nav-link' href="https://luckyswaminathan.github.io/contact">Contact</a>
       <a className='nav-link' href="https://www.linkedin.com/in/lakshman-swaminathan-0208971b1/">LinkedIn</a>
     </nav>
   </aside>
--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,19 +1,64 @@
 import React from "react";
 
 import myImage from './images/koolioai.jpeg';
-
+import dao from './images/franklindao.jpeg';
+import cabrium from './images/cabrium.png';
+import PICS from './images/pics.png';
+import './Experience.css';
 const Experience = () => (
         <div className='text'>
             <h1> 
-                
-                Software Engineering Intern at <a href="http://koolio.ai/">koolio.ai</a>
+                CEO/cofounder at <a href="https://cabrium-one.vercel.app/">Cabrium.ai</a>
 
                 <br></br>
                 <br></br>
-                <img src={myImage} className='img'/>
+
+                <div className="date-image">
+            <span className="date">October 2023 - Present</span>
+            <img src={cabrium} className='img'/>
+                 </div>
                 
-                October 2022 - February 2024
                 
+                <br></br>
+                <br></br>
+                Research Committee Member at <a href="https://www.franklindao.xyz/">Franklin DAO</a>
+                <br></br>
+                <br></br>
+                <div className="date-image">
+            <span className="date">January 2024 - Present   </span>
+            <img src={dao} className='img'/>
+                 </div>
+                
+                <br></br>
+                <br></br>
+            Software Engineering Intern at <a href="http://koolio.ai/">koolio.ai</a>
+                <br></br>
+                <br></br>
+                <div className="date-image">
+            
+            <span className="date">October 2023 - February 2024</span>
+            <img src={myImage} alt="Koolio.ai Logo" className='img'/>
+                 </div>
+                <br></br>
+                <br></br>
+
+                
+                 <br></br>
+                 <br></br>
+                 October 2023 - February 2024
+                
+                
+                <br></br>
+                <br></br>
+
+                Machine Learing Research Intern at <a href="https://lugroup.yale.edu/"> Lu Group</a>
+                <br></br>
+                <br></br>
+                <div className="date-image">
+            <span className="date">April 2023 - October 2023</span>
+            <img src={PICS} className='lu-img'/>
+                 </div>
+
              </h1>
         </div>
     
